5fdd75e5dbe22b98d87d73a04eb284b6b966a117
notalicilaol <oiiinkoinkoink@gmail.com>
2024-04-09 01:47:28 +0000

    fix some configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,36 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "react-refresh",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    browser: true,
+    es2020: true,
+  },
+  ignorePatterns: [".eslintrc.js", "dist", ".eslintrc.cjs"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+import { UserConfigExport } from "vite";
+
+const config: UserConfigExport = defineConfig({
+  logLevel: "error",
   plugins: [react()],
-})
+  define: {
+    "import.meta.env": Object.fromEntries(
+      Object.entries(process.env).map(([key, value]) => [`VITE_${key}`, value])
+    ),
+  },
+  resolve: {
+    alias: {
+      "@components": "/src/components",
+      "@controllers": "/src/controllers",
+      "@pages": "/src/pages",
+      "@routes": "/src/routes",
+      "@styles": "/src/styles",
+      "@stores": "/src/stores",
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:6901",
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+    port: 6900,
+  }
+});
+
+export default config;
