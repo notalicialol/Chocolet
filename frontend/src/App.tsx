0515e54c76dadffd63a60035b4ce97609b666e80
notalicilaol <oiiinkoinkoink@gmail.com>
2024-04-10 14:18:37 +0000

    update construction message (next is scss config)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 export default function App() {
     return (
         <>
-            i am rewriting chocolet in typescript
-            it will be up to speed very soon :)
+            This page is under construction.
         </>
     )
 }
