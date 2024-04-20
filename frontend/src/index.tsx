7f5b5529b039cde75723c7cc6bce1e0e8eefcabf
notalicilaol <oiiinkoinkoink@gmail.com>
2024-04-20 23:04:03 +0000

    fixed error - actually displays content now!

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-createRoot(document.getElementById("app") as HTMLElement).render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById("app") as HTMLElement).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>);
