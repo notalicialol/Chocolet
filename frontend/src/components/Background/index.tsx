5dcd0b22d7a64f970b5f049ad4378cfddd98102e
notalicilaol <oiiinkoinkoink@gmail.com>
2024-04-20 22:10:37 +0000

    background component

---
+++ frontend/src/components/Background/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./background.module.scss";
+
+export default function Background() {
+    return (
+        <div className={styles.background}>
+            <div className={styles.backgroundBlooks}></div>
+        </div>
+    )
+}
